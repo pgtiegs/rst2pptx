be862cbc43a9479abfa3b659a8ec4942ec2526f1
Peter Tiegs <netpete@gmail.com>
2017-09-01 22:27:58 -0700

    Added Readme and setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(
+    name='rst2pptx',
+    version=0.1,
+    scripts=['Rst2pptx.py'],
+    author="Peter Tiegs",
+    author_email="netpete@gmail.com",
+    url="https://netpete.visualstudio.com/Rst2pptx"
+
+    )

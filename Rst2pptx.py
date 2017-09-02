ce8981fa9a7b2cb7cb9541813b5b1bd844b1b6eb
Peter Tiegs <netpete@gmail.com>
2017-09-02 14:24:33 -0700

    Updated some test code

--- Rst2pptx.py
+++ Rst2pptx.py
@@ -1,2 +1,25 @@
+import pptx
+import docutils.core
+import lxml.etree as ET
+
+def get_doctree(rststring):
+    """
+    Return the Doctree XML from the RST string
+    """
+    doctree = docutils.core.publish_string(rststring, writer_name="xml")
+    return doctree
+
+def render(rststring):
+    prs = pptx.Presentation()
+    doctree = get_doctree(rststring)
+    print(doctree)
+    root = ET.fromstring(doctree)
+   
+
+    print(root)
+
+    return prs
+
+
 if __name__ == "__main__":
-	print("Here")
+    print("Here")

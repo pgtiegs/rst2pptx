a0d6fd8c37475d289a67f69a8d0dc5d230d9dfeb
Peter Tiegs <netpete@gmail.com>
2017-09-02 09:28:06 -0700

    Added Install requires

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name='rst2pptx',
     version=0.1,
     scripts=['Rst2pptx.py'],
+    install_requires=['docutils', 'python-pptx'],
     author="Peter Tiegs",
     author_email="netpete@gmail.com",
     url="https://netpete.visualstudio.com/Rst2pptx"

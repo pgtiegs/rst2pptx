6f68794934ae4c585c423281623083bf433722bc
Peter tiegs <netpete@gmail.com>
2017-12-13 23:53:55 -0800

    Move to CMake Wheel generation

--- setup.py
+++ setup.py
@@ -2,12 +2,6 @@
 from setuptools import setup
 
 setup(
-    name='rst2pptx',
-    version=0.1,
     scripts=['Rst2pptx.py'],
     install_requires=['docutils', 'python-pptx'],
-    author="Peter Tiegs",
-    author_email="netpete@gmail.com",
-    url="https://netpete.visualstudio.com/Rst2pptx"
-
     )
